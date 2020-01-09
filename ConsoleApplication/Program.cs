4b190c8d83aac1e348fe00cda9f4ffe55c8e63b9
User <User@LAPTOP-MES>
2020-01-08 21:24:22 -0500

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+
+        public static int FibonacciReverse(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else return FibonacciReverse(n - 2) - FibonacciReverse(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the last number of the sequence (Hint 13): ");
+            int upperBound = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+            while(i < upperBound)
+            {
+                Console.WriteLine(Fibonacci(i));
+                i += 1;
+            }
+            while (i > 0)
+            {
+                Console.WriteLine(FibonacciReverse(i));
+                i -= 1;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ xUnit_Fibonacci/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace xUnit_Fibonacci
+{
+    public class UnitTest1
+    {
+        public static int myFunction(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else return myFunction(n-1) + myFunction(n-2);
+            
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        public void TestMethod1(int expected, int n)
+        {
+            int result = myFunction(n);
+            Assert.Equal(expected, result);
+        }
+    }
+}
